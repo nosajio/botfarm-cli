1094cb7a78c1000bb3cb5c234d4e7629013efd92
Jason Howmans <jason@nosaj.io>
2018-04-04 17:50:07 +0100

    Add runtime to log output

--- cli/actions/logs/fmt.js
+++ cli/actions/logs/fmt.js
@@ -2,11 +2,11 @@ const chalk = require('chalk');
 
 const label = msg => chalk`{bgWhiteBright.black.bold ${msg}}`
 
-const logHeader = (bot_name, time, type) => label(`${bot_name} ${time} ${type} ➜ `);
+const logHeader = (bot_name, time, type, runtime) => label(`${bot_name} ${time} ${type} [${runtime}ms] ➜ `);
 
 function formatLogEntry(log) {
   const time = new Date(log.time);
-  return `${logHeader(log.bot_name, time, log.type)}\n\n${log.output}\n`;
+  return `${logHeader(log.bot_name, time, log.type, log.runtime_ms)}\n\n${log.output}\n`;
 }
 
 module.exports = { label, formatLogEntry };
