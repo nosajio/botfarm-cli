a6dfbb0d6ae9201b975d6a523ed952b5a33bf66a
Jason Howmans <jason@nosaj.io>
2018-04-05 13:15:59 +0100

    Sleep between runs

--- service/runloop.js
+++ service/runloop.js
@@ -4,10 +4,12 @@ const { sleep } = require('sleep');
 const is = require('is_js');
 const db = require('db');
 const queue = require('queue');
+const datefns = require('date-fns');
 const { runner } = require('bots');
 
 // A simple delay using native C++ sleep 
 const wait = async seconds => {
+  debug('💤  Sleeping for %s seconds', seconds);
   sleep(seconds);
   return;
 };
@@ -24,18 +26,39 @@ const requeueBots = ranBots => {
  * Get next due bot's runtime from the head of the queue and use it to 
  * calculate how long the service should sleep until its next needed.
  */
-const sleepUntilNext = () => {
-  // TODO: get next runtime
-  runloop();
+const nextRun = async () => {
+  const queueItems = await db.queue.get();
+  // When the queue is empty, check again in 60 seconds
+  if (is.empty(queueItems)) {
+    const timeIn60s = datefns(new Date()).addMinutes(1);
+    return timeIn60s;
+  }
+  const nextRunTime = new Date(queueItems[0].time);
+  return nextRunTime;
+}
+
+/**
+ * Calculate how many seconds are between now and a future date
+ * @param {Date} date 
+ * @returns {number} Seconds until the date
+ */
+const secsUntilDate = date => {
+  const nowMs = datefns.getTime( new Date() );
+  const thenMs = datefns.getTime(date);
+  const deltaMs = thenMs - nowMs;
+  if (deltaMs < 0) {
+    return 0;
+  }
+  const secondsUntil = Math.ceil(deltaMs / 1000);
+  return secondsUntil;
 }
 
 /**
  * Run through the due bots and run them after a delay
- * @param {Date} nextRuntime When should the runloop resume (seconds)
  */
-const runloop = (nextRuntime = new Date()) => {
-  const secsUntilNextRun = 5;
-  // TODO: compute seconds until next bot is due
+const runloop = async () => {
+  const nextRunTime = await nextRun();
+  const secsUntilNextRun = secsUntilDate(nextRunTime) || 5;
   wait(secsUntilNextRun)
     .then(() => runner.runDueBots())
     .catch(err => error(err))
@@ -43,7 +66,7 @@ const runloop = (nextRuntime = new Date()) => {
     .then(ranBots => requeueBots(ranBots))
     .catch(err => error(err))
     // Finally, re-start the runloop 
-    .then(() => sleepUntilNext())
+    .then(() => runloop())
     .catch(err => error(err));
 }
 
